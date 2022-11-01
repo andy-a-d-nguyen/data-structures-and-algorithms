7c82f71a8f9d8411a6983a5778519fad8d350375
Andy Nguyen <a.d.nguyen.andy@gmail.com>
2022-10-31 21:28:53 -0700

    WIP all tests for stack and queue passing. README next

--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -1,12 +1,42 @@
+from data_structures.invalid_operation_error import InvalidOperationError
+from data_structures.node import Node
+
+
 class Queue:
     """
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, front=None):
+        self.front = front
+        self.back = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def enqueue(self, value):
+        node = Node(value)
+        if self.is_empty():
+            self.front = node
+            self.back = node
+        else:
+            current = self.front
+            while current.next_node is not None:
+                current = current.next_node
+            current.next_node = node
+            self.back = node
+
+    def dequeue(self):
+        if self.is_empty():
+            raise InvalidOperationError("Method not allowed on empty collection")
+        else:
+            dequeued_node = self.front
+            self.front = self.front.next_node
+            return dequeued_node.value
+
+    def peek(self):
+        if self.is_empty():
+            raise InvalidOperationError("Method not allowed on empty collection")
+        else:
+            return self.front.value
+
+    def is_empty(self):
+        return self.front is None
+
--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,12 +1,36 @@
+from data_structures.invalid_operation_error import InvalidOperationError
+from data_structures.node import Node
+
+
 class Stack:
     """
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, top=None):
+        self.top = top
 
-    def some_method(self):
-        # method body here
-        pass
+    def push(self, value):
+        node = Node(value)
+        if self.is_empty():
+            self.top = node
+        else:
+            node.next_node = self.top
+            self.top = node
+
+    def pop(self):
+        if self.is_empty():
+            raise InvalidOperationError("Method not allowed on empty collection")
+        else:
+            popped_node = self.top
+            self.top = self.top.next_node
+            return popped_node.value
+
+    def peek(self):
+        if self.is_empty():
+            raise InvalidOperationError("Method not allowed on empty collection")
+        else:
+            return self.top.value
+
+    def is_empty(self):
+        return self.top is None
