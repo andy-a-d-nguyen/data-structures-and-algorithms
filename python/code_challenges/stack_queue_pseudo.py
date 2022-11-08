bdc0c890f48edd334961ff29a69352cf6660b8d3
Andy Nguyen <a.d.nguyen.andy@gmail.com>
2022-11-07 20:52:23 -0800

    WIP Completed PseudoQueue class implementation

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -1,2 +1,20 @@
+from data_structures.stack_and_queue.stack import Stack
+
+
 class PseudoQueue:
-    pass
+    def __init__(self):
+        self.first_stack = Stack()
+        self.second_stack = Stack()
+
+    def enqueue(self, value):
+        self.first_stack.push(value)
+
+    def dequeue(self):
+        while self.first_stack.is_empty() is False:
+            popped_value = self.first_stack.pop()
+            self.second_stack.push(popped_value)
+        oldest_value = self.second_stack.pop()
+        while self.second_stack.is_empty() is False:
+            popped_value = self.second_stack.pop()
+            self.first_stack.push(popped_value)
+        return oldest_value
