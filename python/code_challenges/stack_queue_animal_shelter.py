bf2d2a78531e9411ff0926d02d6f903224d02bbe
Andy Nguyen <a.d.nguyen.andy@gmail.com>
2022-11-10 20:26:36 -0800

    WIP complete stack_queue_animal_shelter

--- python/code_challenges/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter.py
@@ -1,5 +1,29 @@
+from data_structures.stack_and_queue.queue import Queue
+
+
 class AnimalShelter:
-    pass
+    def __init__(self):
+        self.shelter = Queue()
+
+    def enqueue(self, animal):
+        self.shelter.enqueue(animal)
+
+    def dequeue(self, pref):
+        if pref == "dog" or pref == "cat":
+            animal_type = pref.capitalize()
+            current = self.shelter.front
+            if current.value.__class__.__name__ == animal_type:
+                matching_animal = current.value
+                self.shelter.front = current.next_node
+                return matching_animal
+            else:
+                while current.next_node is not None:
+                    if current.next_node.value.__class__.__name__ == animal_type:
+                        matching_animal = current.next_node.value
+                        current.next_node = current.next_node.next_node
+                        return matching_animal
+                    else:
+                        current = current.next_node
 
 
 class Dog:
