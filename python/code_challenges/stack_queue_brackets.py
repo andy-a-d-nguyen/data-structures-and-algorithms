7ab125362ebdac185e51261a5eb75633da52b392
Andy Nguyen <a.d.nguyen.andy@gmail.com>
2022-11-14 20:24:05 -0800

    WIP Code Challenge Class 13 tests passing

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,2 +1,46 @@
-def multi_bracket_validation():
-    pass
+from data_structures.stack_and_queue.stack import Stack
+
+
+def multi_bracket_validation(string):
+    stack = Stack()
+    bracket_pairs = {
+        "]": "[",
+        "}": "{",
+        ")": "("
+    }
+    for char in string:
+        if char == "[" or char == "{" or char == "(":
+            stack.push(char)
+        elif char in bracket_pairs:
+            if stack.is_empty() is True:
+                return False
+            else:
+                if stack.peek() == bracket_pairs[char]:
+                    stack.pop()
+    return stack.is_empty()
+
+
+def multi_bracket_validation_unoptimized(string):
+    stack = Stack()
+    for char in string:
+        if char == "[" or char == "{" or char == "(":
+            stack.push(char)
+        elif char == "]":
+            if stack.is_empty() is True:
+                return False
+            else:
+                if stack.peek() == "[":
+                    stack.pop()
+        elif char == "}":
+            if stack.is_empty() is True:
+                return False
+            else:
+                if stack.peek() == "{":
+                    stack.pop()
+        elif char == ")":
+            if stack.is_empty() is True:
+                return False
+            else:
+                if stack.peek() == "(":
+                    stack.pop()
+    return stack.is_empty()
